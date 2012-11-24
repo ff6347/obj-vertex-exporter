32e5539c97d2e6ce7d4db31944dacaa2756c96a9
fabiantheblind <fabiantheblind@googlemail.com>
2012-11-24 10:24:37 +0100

    added some comments removed face writing

--- obj-export.jsx
+++ obj-export.jsx
@@ -27,7 +27,7 @@
  * Satya G Meka aka Rowbyte is the only one out of
  * Element 3D, Form and Plexus
  * who seems to get the obj import right
- * I didn't try the atomkraft plugins but
+ * I didn't try the atomkraft plugins.
  * Element 3D and Form fail with the OBJ files created
  * E3D needs faces to import an obj and then the scale.
  * Its pretty wired.
@@ -38,6 +38,36 @@
  * But hey. Still have fun with that script.
  */
 
+
+/**
+ * Videocopilot Element 3D Quirks:  
+ * - E3D needs faces in the obj. If there are only verticies he does not find anything at all
+ * - E3D seems to read only positive values
+ * - E3D scaleing is offseted
+ * - E3D position is offseted
+ * - E3D writing all v's to faces does not solve the problem so it is deactivated
+ * 
+ */
+
+
+/**
+ * Trapcode Form Quirks:  
+ * - Form needs no faces. He finds the verticies
+ * - better shut of normalize
+ * - To get something that is near to but not fully right
+ *   Set the Basefrom x to your obj comps x
+ *   Set the Basefrom y to your obj comps y
+ *   Set the Basefrom z to your obj comps z
+ *   But still. It is not fully right.
+ *   Fiddle with the z offset
+ *   you need to offset the center z of the Base Form
+ * 
+ */
+
+/**
+ * Blender does not like my files either.
+ * 
+ */
 // ------------------------
         run_script(this);
 // ------------------------
@@ -45,10 +75,58 @@
 function run_script(thisObj){
     var objex = new Object();
     objex.settings = {
+      sequence : false,
+      /**
+       * This is a poor attempt to write faces
+       * You cant reach this via UI
+       */
+      writefaces : false 
     };
     objex.helpString = [];
     objex.helpString.push("obj-export.jsx");
-    objex.helpString.push("is a simple oneshot script that takes the positions of all selected layers and writes obj verticies from it. It works out of the box with Plexus by Rowbyte. To use it with Trapcodes Form or Videocopilots Element 3D you have to adjust the the transformation of the obj. As soon as I've figured out how to adjust them I will write this in here.");
+   objex.helpString.push("is a simple oneshot script that takes the positions of all selected layers and writes obj verticies from it. It works out of the box with Plexus by Rowbyte.");
+    objex.helpString.push("/**\
+ * Plexus is the only one out of\
+ * Element 3D, Form and Plexus\
+ * who seems to get the obj import right.\
+ * I didn't try the atomkraft plugins nor others.\
+ * Element 3D and Form get problems with the OBJ files created\
+ * E3D needs faces to import an obj and seems to have problem with negative Z values.\
+ * Its pretty wired. So I disabled that for the moment.\
+ * Form also does not get the world right.\
+ * No offense I love E3D and Form but\
+ * Plexus eats these OBJ files out of the box.\
+ *\
+ * But hey. You still can have fun with this script.\
+ */");
+ 
+
+objex.helpString.push("/**\
+ * Trapcode Form Quirks:  \
+ * - Form needs no faces. He finds the verticies\
+ * - better shut of normalize\
+ * - To get something that is near to but not fully right\
+ *   Set the Basefrom x to your obj comps x\
+ *   Set the Basefrom y to your obj comps y\
+ *   Set the Basefrom z to your obj comps z\
+ *   But still. It is not fully right.\
+ *   Fiddle with the z offset\
+ *   you need to offset the center z of the Base Form\
+ * \
+ */");
+
+objex.helpString.push("/**\
+ * Videocopilot Element 3D Quirks:  \
+ * - E3D needs faces in the obj. If there are only verticies he does not find anything at all\
+ * - E3D seems to read only positive values\
+ * - E3D scaleing is offseted\
+ * - E3D position is offseted\
+ * - E3D writing all v's to faces does not solve the problem so it is deactivated\
+ * \
+ */");
+
+objex.helpString.push("Btw: Blender does not like these files either. ;(");
+
 
 
     var errorStrings = new Object();
@@ -80,14 +158,15 @@ function buildUI (thisObj , objex ,errorStrings, uiStrings) {
         var y = G;
         var yuioff = G;
 
-        win.export_button = win.add('button', [x,y,x+W1*3,y + H], 'export'); 
-        win.help_button = win.add('button', [x + W1*3+G,y,x + W1*4,y + H], '?'); 
+        win.sequence_check = win.add('checkbox',[x,y,x+W1*2,y + H],'seq')
+        win.export_button = win.add('button', [x + W1*2 -G*2,y,x+W1*5,y + H], 'export');
+        win.help_button = win.add('button', [x + W1*5+ G,y,x + W1*6,y + H], '?'); 
 
         win.help_button.onClick = function () {
           alert(objex.helpString.join("\n"));
         }
         win.export_button.onClick = function(){
-          export_obj();
+          export_obj(objex);
         };
 
 
@@ -111,7 +190,7 @@ function buildUI (thisObj , objex ,errorStrings, uiStrings) {
 
 
 // main();
-function export_obj(){
+function export_obj(objex){
 
   app.beginUndoGroup("export obj");
 
@@ -132,17 +211,12 @@ if ((curComp.selectedLayers.length < 1)){
 var cw = curComp.width;
 var ch = curComp.height;
 
-var list = new Array();
-
-// var doc = app.activeDocument;
-// var str = "";
 var coords = new Array();
 
-
-// var ab = doc.artboards[0];
-// ab.rulerOrigin = [0 , 0]; 
-
 var selection = curComp.selectedLayers;
+
+if(objex.settings.sequence == false){
+
 
 
 for(var j in selection){
@@ -151,26 +225,6 @@ var sel = selection[j];
 
 coords.push(get_position(sel));
 
-// for(var i = 0; i < sel.pathPoints.length;i++){
-// if(sel.threeDLayer==false){
-//   var x = sel.transform.position.value[0].toFixed(3);
-//   var y = sel.transform.position.value[1].toFixed(3);
-//   var z = 0.0;
-//   // str = "" + (sel.transform.position.value[0].toFixed(3)) +" " +(sel.transform.position.value[1].toFixed(3)) + " 0.0\r\n";
-//   coords.push("" + x +" " + y + " "+ z);
-
-// }else{
-//     coords.push("" +
-//       (sel.transform.position.value[0].toFixed(3)) +" "+
-//       (sel.transform.position.value[1].toFixed(3)) + " "+
-//       (sel.transform.position.value[2].toFixed(3))
-//       );
-
-// }
-
-
-    // list.push(sel.pathPoints[0].value);
-    // }
   }
 
 var newLocation = Folder.selectDialog("Select a output folder...");
@@ -180,12 +234,15 @@ var newLocation = Folder.selectDialog("Select a output folder...");
   var tf = newLocation.fsName;
   var txtFile =  new File( tf+"/" + fn + ".obj" ); 
 
-// alert(list);
 
 writeArray(txtFile, coords, curComp.name + "" );
-// writeData (txtFile , str  );
-    alert("wrote: " + fn + ".obj\nto folder: " + tf);
 
+alert("wrote: " + fn + ".obj\nto folder: " + tf);
+}else{
+
+  alert("This should write a sequence");
+
+}
 app.endUndoGroup();
 }
 
@@ -216,13 +273,19 @@ return "" + x +" " + y + " "+ z;
       out = txtFile.open( 'e', undefined, undefined );
 
       txtFile.encoding = "UTF-8";
-      if(checkOS()== true){
-        // we are on WIN
+
+    var res = checkOSSave(); 
+      if((res[0] == true)&&(res[1]== false)){
       txtFile.lineFeed = "Windows"; //convert to Windows linefeed
-      }else if (checkOS()== false){
-        // we are on MAC
+      }else if((res[0] == false)&&(res[1]== true)){
+      txtFile.lineFeed = "Unix"; //convert to UNIX linefeed
+      }else{
+      alert("I cant determine your OS.\nI will use Unix linefeeds.");
       txtFile.lineFeed = "Unix"; //convert to UNIX linefeed
       }
+
+
+
       var openString = "# WaveFront *.obj file generated by Adobe After Effects\n"+
                           "# with obj-export.jsx\n\n";
     // var groupName = "g " + grName;
@@ -236,15 +299,35 @@ return "" + x +" " + y + " "+ z;
 
       txtFile.writeln("v " + arr[i] );
      }
+
+     // this is not reachable by UI
+     // 
+    if(objex.settings.writefaces == true){
+     // not fit for E3D
      var e3dface_str = "f ";
-     for(var j =0; j < arr.length;j++){
-      e3dface_str+= " " + String(j+1);
-     }
+      for(var j =0; j < arr.length;j++){
+       e3dface_str+= " " + String(j+1);
+      }
       txtFile.writeln(e3dface_str);
+    };
+
       txtFile.close();   
      // txtFile.execute();  
    }
 };
+
+// found here 
+// http://stackoverflow.com/questions/9223701/math-round-adding-leading-zeros
+
+function pad(number, length) {
+    var str = '' + number;
+    while (str.length < length) {
+        str = '0' + str;
+    }
+
+    return str;
+}
+
 
 function checkOS(){
   var os_string = $.os;
@@ -259,7 +342,13 @@ function checkOS(){
 return bool;
 }
 
-
+function checkOSSave(){ 
+    var os_w = null;
+    var os_m = null;
+    os_w = ($.os.search(/windows/i) != -1) ? true : false;
+    os_m = ($.os.search(/macintosh/i) != -1) ? true : false;
+    return [os_w,os_m];
+} 
 
 function sortfunc(a,b){
 return a - b;
